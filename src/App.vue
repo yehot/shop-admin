597cc84c47cba327110f533248c66977117cc374
yehao <yehao@hashkeytech.com>
2023-09-22 01:46:54 +0800

    添加 windi css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <button class="btn">按钮</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { ElButton } from 'element-plus'
+export default {
+    components: { ElButton },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style>
+.btn {
+    @apply bg-purple-500 text-light-100 px-4 py-2 hover:(bg-purple-300);
 }
 </style>
