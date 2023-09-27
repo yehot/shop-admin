845d51b374e491f6d78f13a106b05ccc72178051
yehao <yehao@hashkeytech.com>
2023-09-28 00:48:54 +0800

    ref 和 reactive 使用

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,31 @@
 <template>
   <el-row style="min-height:100vh;">
-      <el-col :span="16" class="bg-left">1</el-col>
+      <el-col :span="16" class="bg-left">
+        <el-button type="primary" size="default" @click="add"> {{ count }}</el-button>
+        <el-button size="default" @click="addAge"> {{ form.age }}</el-button>
+      </el-col>
       <el-col :span="8" class="bg-right">2</el-col>
   </el-row>
 </template>
 
 <script setup>
+// ref 和 reactive 区别：基础数据类型和
+import { ref, reactive } from 'vue'
+
+let count = ref(1)
+
+const form = reactive({
+  age: 2
+})
+
+function addAge() {
+  form.age++;
+}
+
+function add() {
+  count.value++
+  console.log(count.value)
+}
 </script>
 
 <style>
